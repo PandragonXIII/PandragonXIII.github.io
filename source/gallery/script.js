c506752718d0f2f8cb251f8bb1524a6aba7233c6
XIII <XIII@126.com>
2024-05-01 14:04:57 +0800

    added video button and haimao icon+audio

--- source/gallery/script.js
+++ source/gallery/script.js
@@ -3,15 +3,17 @@ $("#btn").click(function(){
  
     $("#video_window").append(
         <iframe id="video_window iframe"
-        src="https://player.bilibili.com/player.html?aid=1152932500&bvid=BV1YZ421e7BB&cid=1499097259&p=0&as_wide=1" 
-        scrolling="no" 
-        border="0"
-        frameborder="no" 
-        framespacing="0" 
-        allowfullscreen="true" 
-        muted="0"
-        width="91.69%"
-        height="22.58%"
-        > </iframe>)
+      src="https://player.bilibili.com/player.html?aid=1152932500&bvid=BV1YZ421e7BB&cid=1499097259&p=0&t=0
+      &as_wide=1&autoplay=0&muted=0" 
+      scrolling="no" 
+      border="0"
+      frameborder="no" 
+      framespacing="0" 
+      allowfullscreen="true" 
+      autoplay=0
+      muted=0
+      width="91.69%"
+      height="22.58%"
+      > </iframe>)
  
 })
